4eefb5af6bd4a24584e3b448b559ede0133cc1a6
Eduardo Rocha <eduardomacedo1502@gmail.com>
2023-08-24 17:35:39 -0400

    classe funcional

--- php/classe.php
+++ php/classe.php
@@ -13,12 +13,27 @@ class Database {
         $this->password = $password;
         $this->dbname = $dbname;
 
-        $this->conn = $conn = new mysqli($this->$host , $this->$username);
-
-       
+        try {
+            $this->conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            echo "Erro na conexão: " . $e->getMessage();
+        }
     }
 
-    
+    public function select($table) {
+        $sql = "SELECT * FROM $table";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->execute();
+        $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        
+        if ($result) {
+            echo json_encode($result);
+            return $result;
+        }
+        
+        return array(); // Retorna um array vazio se não houver resultados
+    }
 
     public function insert($table, $data) {
         $keys = implode(', ', array_keys($data));
